342aa219c0b751c4141711a2087c89f2c91cb961
unknown <cedric@mendoz.fr>
2024-04-27 08:50:16 +0200

    add mail to secretary

--- controllers/book_controllers.js
+++ controllers/book_controllers.js
@@ -1,9 +1,8 @@
 import bookDB from "../models/schema.js";
 import bookModels from "../models/book_models.js";
 import mail from "../lib/mailing.js";
-import moment from "moment" ;
+import moment from "moment";
 moment.locale("fr");
-
 
 const bookController = {
   /* Cherche tous les RDV  */
@@ -17,7 +16,7 @@ const bookController = {
   /* Compter le nombre de RDV à telle date et telle heure  */
   checkRDV: async (req, res) => {
     try {
-      const { date, heure } = req.body; 
+      const { date, heure } = req.body;
       const count = await bookModels.countReservations(date, heure);
       //console.log("count", count)
       res.json({ count });
@@ -25,19 +24,24 @@ const bookController = {
       //console.error("Erreur lors de la recherche par heure :", error);
       res.status(500).json({ error: error.message });
     }
-    
   },
 
   /* Ajouter réservation */
   create: async (req, res) => {
-    console.log("Ajouter réservation", req.body)
-    const date = moment(req.body.date).format("dddd DD MMMM")
-    console.log("date : ",date)
+    //console.log("Ajouter réservation", req.body)
+    const date = moment(req.body.date).format("dddd DD MMMM");
+    //console.log("date : ",date)
     try {
       const newReservation = await bookModels.createReservation(req.body);
       res.status(201).json(newReservation);
       // envoi du mail
-      mail( req.body.mail,`Confirmation de votre réservation le ${date} à  ${req.body.heure}h `, ); 
+      mail(
+        req.body.mail,
+        `Confirmation de votre réservation le ${date} à ${req.body.heure}h `,
+        "TruckBuster",
+        date,
+        req.body
+      );
     } catch (error) {
       console.error("Erreur lors de l'ajout du rendez-vous :", error);
       res
@@ -45,8 +49,5 @@ const bookController = {
         .json({ message: "Erreur lors de l'ajout du rendez-vous" });
     }
   },
-  
-
-
 };
 export default bookController;
